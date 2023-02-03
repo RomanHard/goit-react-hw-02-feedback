8425526e9ce714fc91250cf0009b672c642bdcf7
Roman <romeenice@gmail.com>
2023-02-03 19:08:15 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import React from 'react';
+import Statistic from '../components/statistic/Statistic.js';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className='feedback'>Give Feedback</h1>
+      
+      <button
+        onClick={() => {
+          console.log('Good');
+        }}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          console.log('Neutral');
+        }}
+      >
+        Neutral
+
+      </button>
+      <button
+        onClick={() => {
+          console.log('Bad');
+        }}
+      >
+        Bad
+      </button>
+        
+      <h1 className='feedback'>Statistics</h1>
+
+      <Statistic good={0} bad={0} total={0} positivePercentage={0} />
     </div>
   );
 };
---
+++ src/components/statistic/Statistic.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './statistic-module.css';
+
+class Statistic extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+}
+
+export default Statistic;
