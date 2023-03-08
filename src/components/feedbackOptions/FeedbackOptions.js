1651d9be67e72f0ced00e316a96947a35dae75ab
Roman <romeenice@gmail.com>
2023-03-08 19:44:10 +0200

    Feedback fix

--- src/components/feedbackOptions/FeedbackOptions.js
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -2,38 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './feedback-module.css';
 
-class FeedbackOptions extends React.Component {
-  render() {
-    const { onLeaveFeedback } = this.props;
-    return (
-      <div className="feedback">
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div className="feedback">
+      {Object.keys(options).map(option => (
         <button
           className="feedback__button"
           type="button"
-          name="good"
+          key={option}
+          name={option}
           onClick={onLeaveFeedback}
         >
-          Good
+          {option}
         </button>
-        <button
-          className="feedback__button"
-          type="button"
-          name="neutral"
-          onClick={onLeaveFeedback}
-        >
-          Neutral
-        </button>
-        <button
-          className="feedback__button"
-          type="button"
-          name="bad"
-          onClick={onLeaveFeedback}
-        >
-          Bad
-        </button>
-      </div>
-    );
-  }
+      ))}
+    </div>
+  );
 }
 
 FeedbackOptions.propTypes = {
